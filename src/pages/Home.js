6690723c4cde36aa1d21b43ef8396d76fbd81825
Eden <gresseden@gmail.com>
2020-04-11 00:43:39 -0400

    generando template primero

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  const view = `
+    <div class="Characters">
+        <article class="Character-item">
+            <a href="#/1/" />
+            <img src="image" alt="image" />
+            <h2>Name</h2>
+        </arcticle>
+    </div>
+    `;
+  return view;
+};
+
+export default Home;
